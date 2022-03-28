98768d4bc10a2b363db3c97a523614adc56ca318
Thomassantosa <Thomassantosa07@gmail.com>
2022-03-28 10:34:35 +0700

    add-.env

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module PBP-API-Tools-1120003-1120011-1120027-1120037-1120043
 
 go 1.17
 
-require gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+require (
+	github.com/joho/godotenv v1.4.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
 
 require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
--- gomail_example.go
+++ gomail_example.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	gomail "gopkg.in/gomail.v2"
 )
 
@@ -10,12 +12,14 @@ func TestGomail() {
 	m.SetHeader("To", "if-20043@students.ithb.ac.id")
 	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	m.SetHeader("Subject", "Test Gomail")
-	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+	m.SetBody("text/html", "Hello, this email was send from <b>Golang</b>!")
 	// m.Attach("/home/Alex/lolcat.jpg")
 
-	d := gomail.NewDialer("smtp.gmail.com", 465, "if-20043@students.ithb.ac.id", "EducationEmail07")
+	emailPassword := LoadEnv("EMAIL_PASS")
+	log.Println(emailPassword)
+	d := gomail.NewDialer("smtp.gmail.com", 465, "if-20043@students.ithb.ac.id", emailPassword)
 
-	// Send the email to Bob, Cora and Dan.
+	// Send the email
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
